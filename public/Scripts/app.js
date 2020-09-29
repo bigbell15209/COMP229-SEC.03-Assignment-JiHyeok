0038103b100c04b694e55189ca859c34ab08b247
John Doe <jkim458@my.centennialcollge.ca>
2020-09-29 10:55:17 -0400

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,15 @@
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+
+
+    function start(){
+        console.log("App Started....");
+    }
+
+
+    window.addEventListener("load",start);
+
+
+
+})();
