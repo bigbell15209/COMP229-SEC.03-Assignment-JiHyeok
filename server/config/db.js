6945bd1fefa4af3799e2423b53ae38f0a4d771db
John Doe <jkim458@my.centennialcollge.ca>
2020-10-22 17:48:52 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = 
 	{
-		"URI": "mongodb://localhost/business_store"
+		"URI": "mongodb+srv://kjd199227:kjd15209@mongodbserver.e33bf.mongodb.net/business_store?retryWrites=true&w=majority"
 	}
